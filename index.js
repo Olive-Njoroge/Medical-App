7b29a8565801e1eb95dabe0cfbb59f9cd1db372e
Olive Njoroge <njorogeolive@gmail.com>
2024-12-22 14:18:58 +0300

    😭😭😭

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 let sidebarButton = document.querySelector("#btn");
 
-function displaySidebar(){
-    let all = document.querySelector(".all");
-    all.style.width = "30px";
-    let span = document.querySelector("span");
-    span.style.display = "none";
-    let navItem = document.querySelector(".nav-item");
-    navItem.style.display = "none";
-    let pT = document.querySelector(".pT");
-    pT.style.display = "none";
-    let tP = document.querySelector(".tP");
-    tP.style.display = "none";
-    let tR = document.querySelector(".tR");
-    tR.style.display = "none";
-    let eC = document.querySelector(".eC");
-    eC.style.display = "none";
-    let tooltip = document.querySelector(".tooltip");
+function showDisplay(){
+    let sidebar = document.querySelector(".sidebar");
+    sidebar.classList.add("all");
+    
 }
 
-sidebarButton.addEventListener ("click", displaySidebar);
+function displaySidebar() {
+  let all = document.querySelector(".all");
+  all.style.width = "30px";
+  let span = document.querySelector("span");
+  span.style.display = "none";
+  let navItem = document.querySelector(".nav-item");
+  navItem.style.display = "none";
+  let pT = document.querySelector(".pT");
+  pT.style.display = "none";
+  let tP = document.querySelector(".tP");
+  tP.style.display = "none";
+  let tR = document.querySelector(".tR");
+  tR.style.display = "none";
+  let eC = document.querySelector(".eC");
+  eC.style.display = "none";
+  let tooltip = document.querySelector(".tooltip");
+}
+
+
+
+sidebarButton.addEventListener("click", () => {
+    displaySidebar();
+    showDisplay();
+});
+
+
+
+
+
+
+
