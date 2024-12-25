ab2a3a1a1200bcec51f50dc8de9275581684bb6d
Olive Njoroge <njorogeolive@gmail.com>
2024-12-25 18:11:41 +0300

    Added a new file

---
+++ API.js
@@ -0,0 +1,4 @@
+let ApiKey = `db36a36a2f66f8114f09od05493b24tc`;
+let prompt = `which is the largest body organ?`;
+let context = `Short precise one word answers`
+let ApiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${ApiKey}`;
